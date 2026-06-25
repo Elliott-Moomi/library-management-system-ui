77be9b572e548ca26be9b6aaa55b8cf93d5bdb21
Elliott Moomi <meselliott7@gmail.com>
2026-06-25 04:19:58 +0200

    Set up circle ci and refactor add pages to be modals

--- src/components/member/MemberTable.jsx
+++ src/components/member/MemberTable.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { MemberCreateContainer } from '../../container/member/MemberCreate.container';
+
 import { Header } from '../header/Header';
 import { MemberRow } from './MemberRow';
 
@@ -10,25 +11,23 @@ export const MemberTable = ({ members }) => {
   return (
     <>
       <Header addBtn="Add Member" onAddClick={() => setIsAddModalOpen(true)} />
-      {isAddModalOpen && (
-        <MemberCreateContainer onClose={() => setIsAddModalOpen(false)} />
-      )}
+      {isAddModalOpen && <MemberCreateContainer onClose={() => setIsAddModalOpen(false)} />}
       <table>
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Age</th>
-          <th>Email</th>
-          <th>Contact</th>
-          <th>Books</th>
-          <th colSpan="3">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <MemberRow members={members} />
-      </tbody>
-    </table>
-  </>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Contact</th>
+            <th>Books</th>
+            <th colSpan="3">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <MemberRow members={members} />
+        </tbody>
+      </table>
+    </>
   );
 };
