43d4ef51bc3d20843240ee4a6bd246d35c416b65
Elliott Moomi <meselliott7@gmail.com>
2026-07-21 02:31:43 +0200

    LMS-10: Integrate with edit member endpoint

--- src/container/member/MemberEdit..container.jsx
+++ src/container/member/MemberEdit..container.jsx
@@ -1,5 +1,13 @@
+import { useDispatch } from 'react-redux';
+
+import { editMember } from '../../state/member/member.slice';
+
 import { MemberEdit } from '../../components/member/MemberEdit';
 
-export const MemberEditContainer = ({ member, onClose }) => (
-  <MemberEdit member={member} onClose={onClose} />
-);
+export const MemberEditContainer = ({ member, onClose }) => {
+  const dispatch = useDispatch();
+  const handleEditMember = (member) => {
+    dispatch(editMember(member));
+  };
+  return <MemberEdit member={member} onClose={onClose} handleEditMember={handleEditMember} />;
+};
